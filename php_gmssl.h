6ed462024af3150f43596c882cf0b506b30d4c1f
jiangjinghui <jiangjinghui@dobest.com>
2025-02-13 17:37:37 +0800

    version

--- php_gmssl.h
+++ php_gmssl.h
@@ -22,7 +22,7 @@
 extern zend_module_entry gmssl_module_entry;
 #define phpext_gmssl_ptr &gmssl_module_entry
 
-#define PHP_GMSSL_VERSION "1.1.0"
+#define PHP_GMSSL_VERSION "1.1.1"
 
 #if defined(ZTS) && defined(COMPILE_DL_GMSSL)
 ZEND_TSRMLS_CACHE_EXTERN()
